4cf667f448889d25694d88ff1b246c936a5f9941
Caladblo <caladblogbaal@gmail.com>
2024-10-20 21:36:01 +0100

    Added the ability to optionally include reply_data in the to_dict method

--- app/models/replay.py
+++ app/models/replay.py
@@ -19,7 +19,7 @@ class Replay(Base):
     p1 = Column(String(255))
     p1_character_id = Column(SMALLINT, CheckConstraint("p1_character_id >= 0 AND p1_character_id <= 35"))
     p2 = Column(String(255), CheckConstraint("p2_character_id >= 0 AND p2_character_id <= 35"))
-    p2_character_id = Column(SMALLINT, CheckConstraint("p1_character_id >= 0 AND p1_character_id <= 35"))
+    p2_character_id = Column(SMALLINT, CheckConstraint("p2_character_id >= 0 AND p2_character_id <= 35"))
     recorder = Column(String(255))
     winner = Column(Integer)
     filename = Column(String(255), unique=True)
@@ -29,11 +29,14 @@ class Replay(Base):
     p2_steamid64 = Column(BIGINT)
     recorder_steamid64 = Column(BIGINT)
 
-    def to_dict(self):
+    def to_dict(self, include_replay_data=False):
         model_dict = {column.name: getattr(self, column.name) for column in self.__table__.columns}
-        # Convert binary data to base64-encoded string
-        replay = model_dict.get("replay")
-        if replay:
-            model_dict["replay"] = base64.b64encode(self.replay).decode("utf-8")
+        if include_replay_data:
+            replay = model_dict.get("replay")
+            if replay:
+                # Convert binary data to base64-encoded string
+                model_dict["replay"] = base64.b64encode(self.replay).decode("utf-8")
+        else:
+            model_dict.pop("replay", None)
 
         return model_dict
