19b8ee0eadfadc32ef84f184b8c81926a4bec820
CaladBlo <caladblogbaal@gmail.com>
2024-12-31 23:27:44 +0000

    new test

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -2,7 +2,7 @@ import pytest
 
 from tests.constants import REPLAYS, REPlAYS_2ND, REPlAYS_3RD
 from tests.helpers import generate_mock_replays_from_data
-from app.utils.helpers import total_up_wins, collapse_replays_into_sets
+from app.utils.helpers import total_up_wins, collapse_replays_into_sets, order_by_cretia_replays
 
 
 @pytest.mark.unit
@@ -25,3 +25,15 @@ def test_collapse_replays_into_sets():
     replays = collapse_replays_into_sets(replays)
 
     assert replays[0]["filename"] == REPLAYS["replays"][-1]["filename"]
+
+
+@pytest.mark.unit
+def test_order_replays_by_options():
+    replays = [replay.to_dict(include_replay_data=False)
+               for replay in generate_mock_replays_from_data(REPLAYS)]
+    order_by_cretia_replays(replays, pos="LEFT", search=("76561199193114720"))
+    assert all(replay["p1"] == "MaddieX3" for replay in replays)
+    order_by_cretia_replays(replays, pos="RIGHT", search=("Maddie"))
+    assert all(replay["p2"] == "MaddieX3" for replay in replays)
+    order_by_cretia_replays(replays, pos="RIGHT", search=("JustATomato"))
+    assert all(replay["p2"] == "JustATomato" for replay in replays)
