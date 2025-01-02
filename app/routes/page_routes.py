72a85c5fe0014e9c0f6f344be29a918e76be181e
CaladBlo <caladblogbaal@gmail.com>
2025-01-02 06:00:15 +0000

    added options route, saving user preferences with cookies

--- app/routes/page_routes.py
+++ app/routes/page_routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, flash, render_template, request
+from datetime import datetime
+
+from flask import Blueprint, flash, render_template, request, make_response
 
 from app.controllers import file_controller
 from app.controllers.file_controller import CorruptedFile
 
 from app.forms.upload import UploadForm
+from app.forms.option import OptionForm
 from app.utils.helpers import clear_cache_on_success
 
 bp = Blueprint("page", __name__, url_prefix="/")
@@ -42,3 +45,28 @@ async def upload():
                 flash(f"Invalid replay data: data is too small/big or corrupted.")
 
     return render_template("upload.html", form=form)
+
+
+@bp.route("/options", methods=["GET", "POST"])
+async def options():
+    form = OptionForm()
+
+    if request.method == "GET":
+        # populate defaults with cookie data
+        for field in form:
+            data = request.cookies.get(field.name, "")
+            if data:
+                field.data = data
+
+    response = make_response(render_template("options.html", form=form))
+
+    if form.validate_on_submit():
+        # manage cookies here
+        for field in form:
+            if field.name != "csrf_token" and field.type not in ["HiddenField", "SubmitField"]:
+                if field.data:
+                    response.set_cookie(field.name, field.data)
+                else:
+                    response.set_cookie(field.name, "", expires=datetime.utcnow())
+
+    return response
