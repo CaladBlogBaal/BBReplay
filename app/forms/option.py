d2dda28c776a78a582ff6dc757e441aa39e13731
CaladBlo <caladblogbaal@gmail.com>
2025-01-06 01:01:50 +0000

    More helpful description for pos

--- app/forms/option.py
+++ app/forms/option.py
@@ -6,7 +6,7 @@ class OptionForm(FlaskForm):
     outcome = SelectField(u"Limit outcomes",
                           render_kw={"id": "outcome_select"},
                           choices=[("", "DEFAULT"), ("WON", "WON"), ("LOST", "LOST")])
-    pos = SelectField(u"Side to sort results to",
+    pos = SelectField(u"Side to sort results to (prioritize P1 if a value is given for both sides )",
                       render_kw={"id": "pos_select"},
                       choices=[("", "DEFAULT"), ("RIGHT", "RIGHT"), ("LEFT", "LEFT")])
     submit = SubmitField("Save")
