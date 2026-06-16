5fcb048f83c7472ca6f234778bfb35a1ef04967d
calad <caladblogbaal@gmail.com>
2026-06-16 04:13:32 +0100

    added time to live to the cache

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -1,12 +1,14 @@
+import time
 from collections import OrderedDict
 
 
 class ReplayCache:
-    __slots__ = ("cache", "capacity")
+    __slots__ = ("cache", "capacity", "ttl")
 
-    def __init__(self, capacity=3000):
+    def __init__(self, capacity=3000, ttl=60):
         self.cache = OrderedDict()
         self.capacity = capacity
+        self.ttl = ttl
 
     @staticmethod
     def _generate_key(params):
@@ -15,13 +17,18 @@ class ReplayCache:
     def get(self, params):
         key = self._generate_key(params)
         if key in self.cache:
+            expires_at, value = self.cache[key]
+            if time.monotonic() > expires_at:
+                del self.cache[key]
+                return
+
             self.cache.move_to_end(key)
-            return self.cache[key]
+            return value
         return
 
     def set(self, params, data):
         key = self._generate_key(params)
-        self.cache[key] = data
+        self.cache[key] = (float("inf") if self.ttl is None else time.monotonic() + self.ttl, data)
         self.cache.move_to_end(key)
         if len(self.cache) > self.capacity:
             self.cache.popitem(last=False)
